392891e4c63a5ed89eb4713ef5b6c4936b56fb40
Sebastiaan van Arkens <sebastiaan@vanarkens.nl>
2017-05-31 20:48:00 +0200

    Ferry is set up!

---
+++ commands.js
@@ -0,0 +1,85 @@
+const ytdl = require('ytdl-core');
+const streamOptions = { seek: 0, volume: 1 };
+
+const botCommandExecuter = function() {}
+
+const findCommandFromStack = (command, callback) => {
+    //Find the command in the commands array
+    commands.some((iteratedCommand) => {
+        //If our keyword is inside the currently iterated command object we have a match
+        if ( iteratedCommand.keywords.indexOf(command) > -1 ) {
+            //Call the callback and break the loop
+            callback(iteratedCommand.action);
+            return true;
+        }
+    });
+}
+
+botCommandExecuter.prototype.execute = (messageInstance, client, parsedMessageContent, broadcastInstance) => {
+    //Find the command
+    findCommandFromStack(parsedMessageContent.command, (commandToExecute) => {
+        //Execute the command we found
+        commandToExecute(messageInstance, client, parsedMessageContent.mentionedBot, broadcastInstance);
+    });
+}
+
+//List of commands
+const commands = [
+    {
+        keywords: ['Toet', 'Toeter', 'boat sound', 'Boat sound'],
+        action: (message, client, mentionedBot, broadcast) => {
+            const voiceChannelConnection = message.member.voiceChannel.connection;
+            if ( voiceChannelConnection ) {
+                const stream = ytdl('https://www.youtube.com/watch?v=BwDuKTvNRBc', { filter : 'audioonly' });
+                broadcast.playStream(stream);
+                const dispatcher = voiceChannelConnection.playBroadcast(broadcast);
+            }
+        }
+    },
+    {
+        keywords: ['Join', 'Kom nou'],
+        action: (message, client, mentionedBot) => {
+            const voiceChannel = message.member.voiceChannel;
+            if ( voiceChannel !== undefined ) {
+                voiceChannel.join()
+                .then(connection => {
+                    console.log('connected');
+                    message.channel.send('Ja ja ja ja, rustig, ik kom er al weer aan.. lul');
+                })
+                .catch(error => {
+                    console.error(error);
+                    message.channel.send('Nee, ik heb nu geen zin om te joinen hoor.')
+                })
+            }
+        }
+    },
+    {
+        keywords: ['ga weg', 'Ga weg', 'rot op', 'Rot op'],
+        action: (message, client, mentionedBot) => {
+            const voiceChannelConnection = message.member.voiceChannel.connection;
+            if ( voiceChannelConnection ) {
+                voiceChannelConnection.disconnect()
+                message.channel.send('Oke ik ga dan wel weer, doet altijd zo lullig');
+            }
+        }
+    },
+    {
+        keywords: ['help', 'Help', 'Ferry handboek', 'ferry handboek'],
+        action: (message, client, mentionedBot) => {
+            message.channel.send('\n\r!Join|!Kom nou - Dan kom ik naar je toe hoor! \n\r !Ga weg|!Rot op - Nou dan ga ik wel weer \n\r !Toet|!Toeter|!Boat sound|!boat sound - Dan ga ik lekker toeteren hoor.');
+        }
+    },
+    {
+        keywords: ['Mongol', 'mongol'],
+        action: (message, client, mentionedBot) => {
+            const responses = [
+                'al die mongols die met nieuw jaar weer gaan sporten',
+                'Lees id gvdmr',
+                'Ga vandaag wel lekker alles lezen over deamon hunter als ik dan de volgende keer speel ben ik niet een mongol'
+            ]
+            message.channel.send(responses[Math.floor(Math.random()*responses.length)]);
+        }
+    }
+];
+
+module.exports = botCommandExecuter;
--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+const commands = require('./commands');
+const prefix = config.prefix;
 
+const broadcast = client.createVoiceBroadcast();
+const commandExecuter = new commands();
 
 client.on("ready", () => {
-    client.user.setGame('Minecraft');
-    var servers = client.guilds.array().map(g =>g.name).join('.');
-    console.log('-------------------\n-[!] Connexion au serveur en cours...\n[!] Veuillez patienter...\nJe suis prête mon capitaine !');
+    client.user.setGame('Hoer simulator');
+    const servers = client.guilds.array().map(g => g.name).join('.');
+    console.log('Bot started');
 });
 
 client.on('message', message => {
-        var tabHello = ['Bonjour','Salut','Hello', 'Guten tag', 'Buenos Dias'];
-        var tabAnsw = ['Bonjour votre majesté.','Salutations jeune Douzien !','Ouais, ouais. T\'es qui déjà ?', 'Bonjour ' + message.author + ', comment vas-tu aujourd\'hui ?'];
-
-        if (tabHello.indexOf(message.content) > -1  && message.isMentioned(client.user)){
-            var row = Math.floor(Math.random() * tabAnsw.length);
-            message.channel.sendMessage(tabAnsw[row]);
-        }  
-    
+    //Check if its a command
+    isBotCommand(message, (parsedMessageConfig) => {
+        //If it is, lets execute it if we can
+        if ( parsedMessageConfig ) {
+            commandExecuter.execute(message, client, parsedMessageConfig, broadcast);
+        }
+    });
 });
+
+const parseMessage = message => {
+    //Lets first get the command
+    const command = message.content.replace(/\<\@.[^\>]+\>/g, '').trim();
+    //check if we are being mentioned
+    const mentionedBot = message.isMentioned(client.user);
+    //return this data
+    return {
+        command,
+        mentionedBot
+    }
+};
+
+const isBotCommand = (message, callback) => {
+    const parsedMessageConfig = parseMessage(message);
+    //Get the first char of the message
+    const firstChar = parsedMessageConfig.command.charAt(0);
+    //If it does not equal our prefix answer that it's not a bot command
+    if (firstChar !== prefix) return callback(false)
+    //Remove that first char from our string
+    parsedMessageConfig.command = parsedMessageConfig.command.substring(1);
+    //We got here, so it seems to be a command
+    return callback(parsedMessageConfig);
+};
 
 client.login(config.token);
